db0f8e0015200b585c7c85c242f77d84a7c7b575
Animels <makfa200222@gmail.com>
2023-03-26 22:05:37 +0300

    22222

--- tests/123.ts
+++ tests/123.ts
@@ -1,11 +1,13 @@
+import {opts} from "../help/index"
+import * as assert from "assert";
+const wdio = require("webdriverio");
+
 async function main () {
 	const client = await wdio.remote(opts);
-
 	const field = await client.$("android.widget.EditText");
 	await field.setValue("Hello World!");
 	const value = await field.getText();
 	assert.strictEqual(value,"Hello World!");
-
 	await client.deleteSession();
 }
 
---
+++ tests/test.ts
@@ -0,0 +1,7 @@
+import {opts} from "../help/index"
+
+function test() {
+    console.log(opts)
+}
+
+ test()
